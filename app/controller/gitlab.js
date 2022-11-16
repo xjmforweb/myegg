bb17a34add64a214e91428d9bb5ae07ee9210406
xjm <zkf15138@163.com>
2022-11-16 13:37:22 +0800

    1

--- app/controller/gitlab.js
+++ app/controller/gitlab.js
@@ -4,8 +4,10 @@ class EsourceController extends Controller {
   async index() {
     const { ctx } = this
     if (ctx.request.body.object_attributes.status === 'success') {
+      const msg = `${ctx.request.body.project.name}[${ctx.request.body.object_attributes.ref}]部署完成(${ctx.request.body.commit.title})`
       // 发消息
-      await ctx.service.gitlab.sendMsg(`eSource部署完成(${ctx.request.body.commit.title})`, ctx.query.touser)
+      console.log(msg)
+      await ctx.service.gitlab.sendMsg(msg, ctx.query.touser)
     }
     ctx.body = 'ok'
   }
